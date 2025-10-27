a417614ba2db83d3ce69ac75017d2115a37ff918
nigmatullayev <ahmadnigmatullayev@gmail.com>
2025-10-27 23:18:11 +0500

    fixed commit

--- model.py
+++ model.py
@@ -1,23 +1,22 @@
-import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+import torch
 
 def load_model():
-    torch.cuda.empty_cache()
     model_id = "openai/gpt-oss-20b"
 
     tokenizer = AutoTokenizer.from_pretrained(model_id)
 
     model = AutoModelForCausalLM.from_pretrained(
         model_id,
-        torch_dtype="auto",
-        device_map="auto"
+        torch_dtype=torch.bfloat16,
+        device_map={"": 0},  # Majburan GPU0ga chiqaramiz
     )
 
     pipe = pipeline(
         "text-generation",
         model=model,
-        tokenizer=tokenizer
+        tokenizer=tokenizer,
+        max_new_tokens=200,
+        temperature=0.7,
     )
-
     return pipe
-
