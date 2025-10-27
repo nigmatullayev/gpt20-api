82c4ca4714518e1a89115f3edc61a7de998ca6c9
nigmatullayev <ahmadnigmatullayev@gmail.com>
2025-10-27 22:53:32 +0500

    fix commit

--- model.py
+++ model.py
@@ -1,12 +1,21 @@
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
 def load_model():
     model_id = "openai/gpt-oss-20b"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        torch_dtype="auto",           # BF16 / FP16 ishlatadi
+        device_map="auto",            # GPUga joylaydi
+        trust_remote_code=True        # GPT-OSS uchun kerak
+    )
+
     pipe = pipeline(
         "text-generation",
-        model=model_id,
-        torch_dtype="auto",
-        device_map="auto",
-        trust_remote_code=True
+        model=model,
+        tokenizer=tokenizer,
+        max_new_tokens=200
     )
     return pipe
